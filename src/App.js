c4a5c6e1bf019415a5008a894839cf513cb1dc8e
nabin <nkbhaie2017@gmail.com>
2022-07-28 10:49:42 -0600

    create a simple component using react state

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import SimpleComponent from './SimpleComponent';
+
+function App() {
+  return (
+  
+    <SimpleComponent />
+  );
+}
+
+export default App;
---
+++ src/SimpleComponent.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const SimpleComponent = () => {
+    const [num, setNum] = useState(0);
+
+    const clickUP = () => {
+        setNum(num + 1);
+    }
+
+    return  (
+        <div>
+            <h3>Count: (num)</h3>
+            <button onClick={clickUP}>Up</button>
+        </div>
+    )
+}
+
+export default SimpleComponent;
