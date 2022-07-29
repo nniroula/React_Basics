e536baa268925257ac9cab01007d98d864e93331
nabin <nkbhaie2017@gmail.com>
2022-07-29 09:02:04 -0600

    add useState hook and a callback to execute it

--- src/SimpleComponent.js
+++ src/SimpleComponent.js
@@ -7,10 +7,19 @@ const SimpleComponent = () => {
         setNum(num + 1);
     }
 
+    // increment number by 2. Simple way is num +2, but using the state another way is:
+    const clickUpBy2 = () => {
+        setNum(n => n + 1);
+        setNum(n => n + 1);
+    }
+
     return  (
         <div>
             <h3>Count: {num}</h3>
             <button onClick={clickUP}>Up</button>
+            <br></br>
+            <br></br>
+            <button onClick={clickUpBy2}>UpBy2</button>
         </div>
     )
 }
